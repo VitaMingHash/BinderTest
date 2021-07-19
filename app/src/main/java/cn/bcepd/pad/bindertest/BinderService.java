c123968c709e5bfc85465a525ae94664c2473eb1
ming <https://gitee.com/zhu_tianming>
2021-07-19 14:52:25 +0800

    t

--- app/src/main/java/cn/bcepd/pad/bindertest/BinderService.java
+++ app/src/main/java/cn/bcepd/pad/bindertest/BinderService.java
@@ -16,14 +16,6 @@ public class BinderService extends Service {
     @Nullable
     @Override
     public IBinder onBind(Intent intent) {
-        BinderManager binderManager = BinderManager.getInstance();
-        IBinder iBinder = binderManager.find(Constant.B);
-        BInterface aInterface = BInterface.Stub.asInterface(iBinder);
-        try {
-            aInterface.b(2);
-        } catch (RemoteException e) {
-            e.printStackTrace();
-        }
         return new BinderManager.IOneInterfaceN();
     }
 }
